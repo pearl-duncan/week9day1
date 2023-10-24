ce24e47ad64cf9d65f0822c459d621c6ae0db867
Pearl Osborne <pearlosborne@hotmail.com>
2023-10-24 16:30:56 -0500

    day 1

--- src/Footer.js
+++ src/Footer.js
@@ -4,11 +4,11 @@ export default class Footer extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar fixed-bottom bg-body-tertiary">
+        <footer class="navbar fixed-bottom bg-body-tertiary" id='footer'>
             <div class="container-fluid">
-            <a class="navbar-brand" href="#">Fixed bottom</a>
-        </div>
-    </nav>
+                <a class="navbar-brand" href="#">Fixed bottom</a>
+            </div>
+        </footer>
       </div>
     )
   }
